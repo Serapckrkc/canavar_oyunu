c86e086bf4260b4dc075e53b2dbe43c61dcde05d
Serapckrkc <Serapckrkc@gmail.com>
2021-07-26 14:22:20 +0300

    File Created

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <section class="row">
+        <div class="small-6 columns">
+            <h1 class="text-center">SEN</h1>
+            <div class="healthbar">
+                <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
+
+                </div>
+            </div>
+        </div>
+        <div class="small-6 columns">
+            <h1 class="text-center">CANAVAR</h1>
+            <div class="healthbar">
+                <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="row controls">
+        <div class="small-12 columns">
+            <button id="start-game">YENİ OYUN</button>
+        </div>
+    </section>
+    <section class="row controls">
+        <div class="small-12 columns">
+            <button id="attack">SALDIRI</button>
+            <button id="special-attack">ÖZEL SALDIRI</button>
+            <button id="heal">İLK YARDIM</button>
+            <button id="give-up">PES ET!</button>
+        </div>
+    </section>
+    <section class="row log">
+        <div class="small-12 columns">
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data (){
+    return{
+
+    }
+  },
+  
+ 
 }
 </script>
 
